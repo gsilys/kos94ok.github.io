4a89262b35944d46a0e55f1a8d8940d48a84657d
Kos94ok <kos94ok@gmail.com>
2016-11-19 13:10:01 +0200

    Added grade, improved basic UI

--- core.js
+++ core.js
@@ -40,10 +40,12 @@ var ExamPoints = 0;
 //=====================================================================
 function Initialization()
 {
+	// Hide home area
+	document.getElementById("Home").style.display = "none";
 	// Generate some temporary levels
 	for (var i = 2; i < 50; i++)
 	{
-		GlobalLevels.push(new Level("Linear", i, i, "Linear"));
+		GlobalLevels.push(new Level("Temporary Exam", i, i, "Linear"));
 	}
 	// Update the UI initially
 	UpdateExamPoints();
@@ -64,16 +66,60 @@ function WorkHard_OnClick()
 
 function EndExam_OnClick()
 {
-	ExamPoints = 0;
-	TotalExamsPassed += 1;
-	CurrentExamOrdinal += 1;
-	UpdateExamPoints();
-	UpdateExamOrdinal();
+	// Display home
+	document.getElementById("Exam").style.display = "none";
+	document.getElementById("Home").style.display = "block";
+	// Update the stats
+	var Grade = GetCurrentExamGrade();
+	if (Grade == 0) {
+		TotalExamsFailed += 1;
+	}
+	else {
+		TotalExamsPassed += 1;
+	}
+	// Update the UI
+	UpdateHome();
 }
 
 function NextExam_OnClick()
 {
+	document.getElementById("Home").style.display = "none";
+	document.getElementById("Exam").style.display = "block";
+	if (CurrentExamOrdinal < GlobalLevels.length - 1)
+	{
+		ExamPoints = 0;
+		TotalExamsPassed += 1;
+		CurrentExamOrdinal += 1;
+		UpdateExamPoints();
+		UpdateExamOrdinal();
+	}
+}
 
+//=====================================================================
+// Supplementary functions
+//=====================================================================
+function GetCurrentExamGrade()
+{
+	var Grade = 5;
+	for (var i = 0; i < 5; i++) {
+		if (ExamPoints < GlobalLevels[CurrentExamOrdinal].Points[i]) {
+			Grade = i;
+			break;
+		}
+	}
+	return Grade;
+}
+
+function GetCurrentExamPointsGoal()
+{
+	var Goal = GlobalLevels[CurrentExamOrdinal].Points[4];
+	for (var i = 0; i < 5; i++) {
+		if (ExamPoints < GlobalLevels[CurrentExamOrdinal].Points[i]) {
+			Goal = GlobalLevels[CurrentExamOrdinal].Points[i];
+			break;
+		}
+	}
+	return Goal;
 }
 
 //=====================================================================
@@ -81,26 +127,37 @@ function NextExam_OnClick()
 //=====================================================================
 function UpdateExamPoints()
 {
-	// Calculate the goal
-	var PointsGoal = 0;
-	for (var i = 3; i >= 0; i--)
-	{
-		if (ExamPoints >= GlobalLevels[CurrentExamOrdinal].Points[i])
-		{
-			PointsGoal = GlobalLevels[CurrentExamOrdinal].Points[i + 1];
-			break;
-		}
-		else if (i == 0)
-		{
-			PointsGoal = GlobalLevels[CurrentExamOrdinal].Points[0];
-		}
-	}
+	// Calculate the goal and grade
+	var Grade = GetCurrentExamGrade();
+	var PointsGoal = GetCurrentExamPointsGoal();
 	// Update the UI
 	document.getElementById("ExamPointsCurrent").innerHTML = ExamPoints;
 	document.getElementById("ExamPointsMax").innerHTML = PointsGoal;
+	document.getElementById("ExamGradeValue").innerHTML = Grade;
+	// Grade color code
+	if (Grade == 0) {
+		document.getElementById("ExamGradeValue").style.color = "red";
+	}
+	else if (Grade < 5) {
+		document.getElementById("ExamGradeValue").style.color = "orange";
+	}
+	else if (Grade == 5) {
+		document.getElementById("ExamGradeValue").style.color = "green";
+	}
 }
 
 function UpdateExamOrdinal()
 {
-	document.getElementById("ExamNumber").innerHTML = "#" + (CurrentExamOrdinal + 1);
+	document.getElementById("ExamName").innerHTML = GlobalLevels[CurrentExamOrdinal].Name;
+	document.getElementById("ExamNumber").innerHTML = "[№" + (CurrentExamOrdinal + 1) + "]";
+}
+
+function UpdateHome()
+{
+	var Grade = GetCurrentExamGrade();
+	var ExamStatus;
+	if (Grade == 0) {
+		ExamStatus = "You have failed the exam!";
+	}
+	document.getElementById("HomeExamStatus").innerHTML = ExamStatus;
 }
